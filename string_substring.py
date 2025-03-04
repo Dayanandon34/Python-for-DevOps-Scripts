7dae0fec2456f09cc2426e2639f1c74932a026bf
Dayanandon34 <Dayanando34@gmail.com>
2025-03-04 20:58:50 +0530

    added python string subtring file

---
+++ string_substring.py
@@ -0,0 +1,10 @@
+
+
+text = "Python is devops" # you can change the sentence add "for" and remove before executing the script.
+substring = "for"
+substring_text = "for"
+
+if substring in text:
+    print(substring, "found in the text")
+else:
+            print(substring_text, "not found")
