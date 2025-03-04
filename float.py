350625bdb802cf1d3d8d54c29f6ca86e468519eb
Dayanandon34 <Dayanando34@gmail.com>
2025-03-04 21:16:13 +0530

    added float python file

---
+++ float.py
@@ -0,0 +1,11 @@
+
+
+num1 = 2.5
+num2 = 3.5
+
+result1 = num1 + num2
+print("addition", result1)
+print(" ")                                      # you can add more arthmetic math and execute like- divide, multiple
+print("next substractoin answer as follows:")
+result2 = num1 - num2
+print("substraction is", result2)
