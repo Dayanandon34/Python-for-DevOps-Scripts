92b27d12ee8336d37bafef4ed11b338589b70ade
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-06 12:12:17 +0530

    Update int.py

--- int.py
+++ int.py
@@ -6,7 +6,7 @@ num2 = 5
 result = num1 + num2     
 print("addition", result)
 
-#You can add remaining arithmetic function add try to execute
+#You can add remaining arithmetic function add try to execute eg- diigion, multiplication etc
 
 result1 = num1 - num2    
 print("minus", result1)  
