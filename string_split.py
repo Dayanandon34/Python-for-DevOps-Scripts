2d84b59e02e0dc94558f761786ef5326d5c43b54
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-09 00:56:02 +0530

    Update string_split.py

--- string_split.py
+++ string_split.py
@@ -1,7 +1,7 @@
 # This python scripts will execute us the split the words from sentence||
 
 
-text = "Python for DevOps"
+text = "Python for DevOps" # enter any text
 
-words = text.split()
+words = text.split() # the syntax for word split
 print ("words", words)
