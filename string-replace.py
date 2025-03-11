006f9b0ee29dea3d7139935058c38ac963dd4a0b
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-11 20:58:54 +0530

    Update string-replace.py

--- string-replace.py
+++ string-replace.py
@@ -1,7 +1,7 @@
 # This script will replace the text using veriable
 
 text = "Python script for shell-scripting"
-new_text = text.replace("shell-scripting", "DevOps")
+new_text = text.replace("shell-scripting", "DevOps") # syntax for replacing the word
 
 print("modified test", new_text)
 
