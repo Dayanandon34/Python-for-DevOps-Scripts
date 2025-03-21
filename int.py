67d29190ef1e4e94b2a2b2f6d31ad5c617ad413b
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-21 08:50:40 +0530

    Update int.py

--- int.py
+++ int.py
@@ -8,5 +8,5 @@ print("addition", result)
 
 #You can add remaining arithmetic function add try to execute eg- diigion, multiplication etc
 
-result1 = num1 - num2    
+result1 = num1 - num2    #syntax for substraction
 print("Substraction", result1)  
