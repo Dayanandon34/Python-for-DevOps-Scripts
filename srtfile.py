1a2cce7af3e6dda82ca6fe497443d39e99ce96f5
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-03-23 12:47:16 +0530

    Update srtfile.py

--- srtfile.py
+++ srtfile.py
@@ -1,6 +1,6 @@
 #added to string and printing the result
 str1 = "Hello" #veriable declartion
-str2 = "Dayanand"
+str2 = "Dayanand" #veriable declartion
 
 result = str1 + " " + str2 #syntax
 print(result)
