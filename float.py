ae039313cb2e6c463160289cf897e9668b52b219
Dayanand Maraganache <123573500+Dayanandon34@users.noreply.github.com>
2025-04-15 20:05:48 +0530

    Update float.py

--- float.py
+++ float.py
@@ -5,7 +5,7 @@ num2 = 3.5
 
 result1 = num1 + num2 #syntex
 print("addition", result1)
-print(" ")                                      # you can add more arthmetic math and execute like- divide, multiple
+print(" ")                                      #you can add more arthmetic math and execute like- divide, multiple
 print("next substractoin answer as follows:")
 result2 = num1 - num2
 print("substraction is", result2)
